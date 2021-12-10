855b6a78c002ee72f7f1efdcb938de56c97c91dd
Thrayz <88392163+Thrayz@users.noreply.github.com>
2021-12-10 18:43:24 +0100

    Add files via upload

---
+++ helpers/head_helper.php
@@ -0,0 +1,8 @@
+
+link href=httpsmaxcdn.bootstrapcdn.combootstrap3.3.7cssbootstrap.min.css rel=stylesheet integrity=sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTszK68vbdEjh4u crossorigin=anonymous
+
+link rel=stylesheet href=httpscode.jquery.comui1.12.1themesui-lightnessjquery-ui.css
+
+script src=httpscode.jquery.comjquery-3.2.1.js integrity=sha256-DZAnKJ6XZ9si04Hgrsxu8s717jcIzLy3oi35EouyE= crossorigin=anonymousscript
+
+script src=httpscode.jquery.comui1.12.1jquery-ui.js integrity=sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30= crossorigin=anonymousscript
